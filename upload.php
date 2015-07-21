71d0f6e08b34bc077c3deaeec1ce3fe3d3dae8ab
khalid <devkhalidfahim@gmail.com>
2015-07-21 05:51:16 +0100

    add all files

---
+++ upload.php
@@ -0,0 +1,38 @@
+<?php
+var_dump($_POST);
+// allowed extension
+$allowedExts = array("gif", "jpeg", "jpg", "png");
+
+// get file extension
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/jpg")
+|| ($_FILES["file"]["type"] == "image/pjpeg")
+|| ($_FILES["file"]["type"] == "image/x-png")
+|| ($_FILES["file"]["type"] == "image/png"))
+&& ($_FILES["file"]["size"] < 200000)
+&& in_array($extension, $allowedExts)) {
+    if ($_FILES["file"]["error"] > 0) {
+        echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    } else {
+        $filename = $_FILES["file"]["name"];
+        echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+        echo "Type: " . $_FILES["file"]["type"] . "<br>";
+        echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+        echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+        if (file_exists("uploads/" . $filename)) {
+            echo $filename . " already exists. ";
+        } else {
+            move_uploaded_file($_FILES["file"]["tmp_name"],
+            "uploads/" . $filename);
+            echo "Stored in: " . "uploads/" . $filename;
+        }
+    }
+} else {
+    echo "Invalid file";
+}
+?>
